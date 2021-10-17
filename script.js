ef480d4befd63226e987da6d466994e6b1dacc2b
SzkolenieFrontEnd <92541764+SzkolenieFrontEnd@users.noreply.github.com>
2021-10-17 21:38:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Nasza udawana baza danych
+const imagesData = [
+    {
+        title: "Zapraszamy na zakupy",
+        desc: "Tylko u nas codziennie niskie ceny",
+        url: '/images/image1.jpg'
+    },
+    {
+        title: "Zobacz najnowsze produkty",
+        desc: "Nowości, których nie znajdziesz nigdzie indziej!",
+        url: '/images/image2.jpg'
+    },
+    {
+        title: "Spersonalizuj swoje zamówienie",
+        desc: "Skontaktuj się z nami i zamów towar specjalnie dla Ciebie!",
+        url: '/images/image3.jpg'
+    }
+];
+
+//Pobierz elementy, wykorzystaj event listenery i warunki if lub pętle
